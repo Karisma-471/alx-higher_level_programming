84ba7e4479f36314e844bf8c8080e40990540638
root <okoyecharisma@gmail.com>
2023-09-13 12:38:19 -0700

    corrected some codes

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-def copy_list(my_list):
-    # Create a shallow copy of the input my_list using slice notation
-    return my_list[:]
+def copy_list(MyList):
+    # Create a shallow copy of the input list l using slice notation
+    return MyList
