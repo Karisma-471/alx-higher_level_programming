ce43ec00c8ef2a849bfb5145bb6b1b80f8e61bb2
Karisma-471 <okoyecharisma@gmail.com>
2023-09-30 22:07:15 +0100

    updating

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,12 @@
-#!/usr/bin/python3
-"""a module that opens and reads a file"""
+!/usr/bin/python3
+"""
+function that reads a text file
+"""
+
 
 def read_file(filename=""):
-    """a function that read a file"""
-    with open(filename, "r", encoding="utf-8") as file:
-        for line in file:
-            print(line, end="")
+    """reads a text file (UTF8) and prints it to stdout
+    Returns none
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
